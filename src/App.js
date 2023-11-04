5b98f3e003bb2c066d0022b9286cfdcaee1001ec
dasari989 <prashanthdasari0503@gmail.com>
2023-11-04 15:23:45 +0530

    commits done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './app.css'
+import './App.css'
 import Main from './components/Main/Main'
 import Home from './components/Home/Home'
 import Footer from './components/Footer/Footer'
